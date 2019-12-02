512f0ddab2bbda14f9b6a7cf7c08d6780b11e79d
Tran Ly <tranly237@gmail.com>
2019-12-02 18:23:40 +0700

    create form add admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import LoginPageContainer from './components/LoginPage/LoginPage.container'
 import 'antd/dist/antd.css'
 import HomePageComponent from './components/HomePage/HomePage.component'
+import CreateAdminComponent from './components/CreateAdmin/CreateAdmin.component'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePageComponent} />
         <Route path="/login" component={LoginPageContainer} />
+        <Route path="/account/create" component={CreateAdminComponent} />
       </Switch>
     </div>
   )
--- src/components/HomePage/HomePage.component.jsx
+++ src/components/HomePage/HomePage.component.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import { Form } from 'antd'
 
 const HomePageComponent = () => {
   return <div>This is home</div>
 }
 
-export default Form.create({ name: 'LoginForm' })(HomePageComponent)
+export default HomePageComponent
