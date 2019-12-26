1b276aeea966210ebd58f50e99470b63954f7f69
Tran Ly <tranly237@gmail.com>
2019-12-26 14:13:43 +0700

    fix money

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,8 +1,8 @@
-const apiUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://tutor-back-end-admin.herokuapp.com'
-    : 'http://localhost:5000'
-
+// const apiUrl =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://tutor-back-end-admin.herokuapp.com'
+//     : 'http://localhost:5000'
+const apiUrl = 'https://tutor-back-end-admin.herokuapp.com'
 console.log('in api-url, process env: ', process.env.NODE_ENV)
 
 export default apiUrl
